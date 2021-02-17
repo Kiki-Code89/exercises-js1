b3e32da24e283e892c32fcd44b6912a70c8d118c
Kiki-Code89 <kikelomobewaji@gmail.com>
2021-02-17 23:41:47 +0100

    java-homework

--- week-1/Homework/F-strings-methods/exercise.js
+++ week-1/Homework/F-strings-methods/exercise.js
@@ -1,3 +1,9 @@
 // Start by creating a variable `message`
-
+let name = "Kiki";
+console.log(name);
+let namelength = name.length;
+console.log(namelength); 
+var start = "my name is";
+var end = "characters long";
+let message = start + " "+ name  + " and " + start + " " + namelength + " "+ end;
 console.log(message);
--- week-1/Homework/F-strings-methods/exercise2.js
+++ week-1/Homework/F-strings-methods/exercise2.js
@@ -1,3 +1,9 @@
 const name = " Daniel  ";
+let namelength = name.length;
+console.log(namelength); 
+var start = "my name is";
+var end = "characters long";
+let message = start + " "+ name.trim() + " and " + start + " " + namelength + " "+ end;
+
 
 console.log(message);
